85b661e6c7f01bb05b25c8e0970e685149bc17a0
Ammonation <harry_a99@hotmail.co.uk>
2018-12-06 15:43:56 +0000

    Created and tested method that checks if the board is unique

--- src/NQueenProblem.java
+++ src/NQueenProblem.java
@@ -1,6 +1,10 @@
-public class NQueenProblem {
-	public NQueenProblem(int n) {
+import java.util.ArrayList;
 
+public class NQueenProblem {
+	int boardSize;
+	ArrayList<ChessBoard> Boards = new ArrayList<ChessBoard>();
+	public NQueenProblem(int n) {
+		boardSize = n;
 	}
 	
 	
@@ -16,7 +20,7 @@ public class NQueenProblem {
 		}
 		
 		// If all queens have been placed then return true
-		if (col >= (n.getBoardWidth())) {
+		if (col >= n.getBoardWidth()) {
 			return true;
 		}
 		
@@ -46,8 +50,8 @@ public class NQueenProblem {
 		}
 		
 		// If all queens have been placed, then problem is solved
-		
-		if (col >= n.getBoardWidth() ) {
+		// If all queens have been placed AND its a unique solution
+		if (col >= n.getBoardWidth() && solutionIsUnique(n) ) {
 			return true;
 		}
 		
@@ -74,6 +78,60 @@ public class NQueenProblem {
 		
 		return false;
 	}
+	
+	private boolean solutionIsUnique(ChessBoard n) {
+		ChessBoard toCheck;
+		for(int i = 0;i<Boards.size();i++) {
+			toCheck = Boards.get(i);
+			for (int j = 0; j<=boardSize; j++) {
+				for (int k = 0; k<=boardSize; k++) {
+					if(toCheck.getPosition(j, k) == n.getPosition(j, k)) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	public void testIsUnique() {
+		System.out.println("Creating new board");
+		ChessBoard board0 = new ChessBoard(boardSize);
+		for (int i = 0;i<board0.getBoardWidth();i++) {
+			board0.setPosition(true, i, i);
+		}
+		System.out.println("board0 created:");
+		Boards.add(board0);
+		board0.display();
+		
+		System.out.println("Creating new board");
+		ChessBoard board1 = new ChessBoard(boardSize);
+		for (int i = 0;i<board1.getBoardWidth();i++) {
+			board1.setPosition(true, i, i);
+		}
+		System.out.println("board1 created:");
+		board1.display();
+		
+		if(solutionIsUnique(board1)) {
+			System.out.println("Boards are unique");
+		} else {
+			System.out.println("NOT UNIQUE");
+		}
+		
+	}
+	
+	
+	public boolean findAllSolutions(int[] userCoord) {
+		Boards.add(new ChessBoard(boardSize));
+		Boards.get(Boards.size()-1).setPosition(true, userCoord[0], userCoord[1]);
+		if(solveAllNQueen(Boards.get(Boards.size()-1), 0, userCoord[0])) {
+			Boards.get(0).display();
+			findAllSolutions(userCoord);
+			return true;
+		}
+		return false;
+	}
+	
 	public boolean isQueenSafe(ChessBoard n, int xCoord, int yCoord) {
 		
 		// Checks if another queen is on the same column
